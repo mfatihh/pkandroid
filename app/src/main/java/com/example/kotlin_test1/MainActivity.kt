7759823e3727554fdafa2dd5fb8fbe1f66abeab2
Muhammad Fatih <m.fatihabdulazis@gmail.com>
2019-06-01 13:39:00 +0700

    First Commit Master

---
+++ app/src/main/java/com/example/kotlin_test1/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kotlin_test1
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+    }
+}
